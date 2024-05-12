355e478c6c65fca58e865a4bb34b2a5bbad75b5c
Florian Brucker <mail@florianbrucker.de>
2024-05-12 19:58:08 +0200

    Basic petgraph demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use petgraph::{
+    algo::min_spanning_tree,
+    data::FromElements,
+    dot::{Config, Dot},
+    graph::UnGraph,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let g = UnGraph::<i32, ()>::from_edges([(1, 2), (2, 3), (3, 4), (1, 4)]);
+
+    let mst = UnGraph::<_, _>::from_elements(min_spanning_tree(&g));
+
+    println!("{:?}", Dot::with_config(&mst, &[Config::EdgeNoLabel]));
 }
